d1230d17fa13749db7c83f46d423763bba74e1af
dariogguillen <guillendario@gmail.com>
2024-12-02 20:23:06 -0600

    feat: operations added

--- src/presentation/hooks/useCalculator.ts
+++ src/presentation/hooks/useCalculator.ts
@@ -1,9 +1,20 @@
-import {useState} from 'react';
+import {useRef, useState} from 'react';
+
+enum Operator {
+  sum = '+',
+  sub = '-',
+  mul = '*',
+  div = '/',
+}
 
 const useCalculator = () => {
   const [expresion, setExpresion] = useState('0');
 
-  const buildExpresion = (str: string) => {
+  const [result, setResult] = useState('');
+
+  const lastOperation = useRef<Operator>();
+
+  const buildExpresion = (str: string | Operator) => {
     if (expresion.includes('.') && str === '.') {
       return;
     }
@@ -32,6 +43,7 @@ const useCalculator = () => {
 
   const softReset = () => {
     setExpresion('0');
+    setResult('');
   };
 
   const deleteLast = () => {
@@ -51,14 +63,53 @@ const useCalculator = () => {
     setExpresion('-' + expresion);
   };
 
+  //last character not an operator
+  const validateLastOperator = (): boolean => {
+    const lastStr = expresion.slice(-1);
+    return !Object.values(Operator).includes(lastStr as Operator);
+  };
+
+  const add = () => {
+    lastOperation.current = Operator.sum;
+    if (validateLastOperator()) {
+      buildExpresion(Operator.sum);
+    }
+  };
+
+  const sub = () => {
+    lastOperation.current = Operator.sub;
+    if (validateLastOperator()) {
+      buildExpresion(Operator.sub);
+    }
+  };
+
+  const mul = () => {
+    lastOperation.current = Operator.mul;
+    if (validateLastOperator()) {
+      buildExpresion(Operator.mul);
+    }
+  };
+
+  const div = () => {
+    lastOperation.current = Operator.div;
+    if (validateLastOperator()) {
+      buildExpresion(Operator.div);
+    }
+  };
+
   return {
     // props
     expresion,
+    result,
     // methods
     buildExpresion,
     softReset,
     deleteLast,
     toggleSign,
+    add,
+    sub,
+    mul,
+    div,
   };
 };
 
--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -5,20 +5,41 @@ import CalculatorButton, {ButtonProps} from '../components/CalculatorButton';
 import useCalculator from '../hooks';
 
 const CalculatorScreen = () => {
-  const {expresion, buildExpresion, softReset, deleteLast, toggleSign} =
-    useCalculator();
+  const {
+    expresion,
+    result,
+    buildExpresion,
+    softReset,
+    deleteLast,
+    toggleSign,
+    add,
+    sub,
+    mul,
+    div,
+  } = useCalculator();
   return (
     <View style={globalStyles.calculatorContainer}>
       <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
+        <Text adjustsFontSizeToFit style={globalStyles.subResult}>
+          {expresion}
+        </Text>
         <Text
           adjustsFontSizeToFit
           numberOfLines={1}
           style={globalStyles.mainResult}>
-          {expresion}
+          {result}
         </Text>
-        <Text style={globalStyles.subResult}>15</Text>
       </View>
-      {buttons(buildExpresion, softReset, deleteLast, toggleSign).map(row => (
+      {buttons(
+        buildExpresion,
+        softReset,
+        deleteLast,
+        toggleSign,
+        add,
+        sub,
+        mul,
+        div,
+      ).map(row => (
         <View key={row[0].label} style={globalStyles.row}>
           {row.map(button => (
             <CalculatorButton key={button.label} {...button} />
@@ -34,30 +55,34 @@ const buttons = (
   softReset: () => void,
   deleteLast: () => void,
   toggleSign: () => void,
+  add: () => void,
+  sub: () => void,
+  mul: () => void,
+  div: () => void,
 ): RowButton[] => [
   [
-    {onPress: () => softReset(), label: 'C', color: colors.lightGray},
-    {onPress: () => toggleSign(), label: '+/-', color: colors.lightGray},
-    {onPress: () => deleteLast(), label: 'del', color: colors.lightGray},
-    {onPress: () => console.log('/'), label: '/', color: colors.orange},
+    {onPress: softReset, label: 'C', color: colors.lightGray},
+    {onPress: toggleSign, label: '+/-', color: colors.lightGray},
+    {onPress: deleteLast, label: 'del', color: colors.lightGray},
+    {onPress: div, label: '/', color: colors.orange},
   ],
   [
     {onPress: () => buildExpresion('7'), label: '7'},
     {onPress: () => buildExpresion('8'), label: '8'},
     {onPress: () => buildExpresion('9'), label: '9'},
-    {onPress: () => console.log('X'), label: 'X', color: colors.orange},
+    {onPress: mul, label: 'X', color: colors.orange},
   ],
   [
     {onPress: () => buildExpresion('4'), label: '4'},
     {onPress: () => buildExpresion('5'), label: '5'},
     {onPress: () => buildExpresion('6'), label: '6'},
-    {onPress: () => console.log('-'), label: '-', color: colors.orange},
+    {onPress: sub, label: '-', color: colors.orange},
   ],
   [
     {onPress: () => buildExpresion('1'), label: '1'},
     {onPress: () => buildExpresion('2'), label: '2'},
     {onPress: () => buildExpresion('3'), label: '3'},
-    {onPress: () => console.log('+'), label: '+', color: colors.orange},
+    {onPress: add, label: '+', color: colors.orange},
   ],
   [
     {onPress: () => buildExpresion('0'), label: '0', doubleSize: true},
